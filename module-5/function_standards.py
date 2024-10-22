fa260a3fc68c217a2a012993a629a1ea649e285f
Damien Altenburg <daltenburg@rrc.ca>
2024-10-22 11:11:43 -0500

    Update to demonstrate function documentation.

--- module-5/function_standards.py
+++ module-5/function_standards.py
@@ -7,7 +7,20 @@ Example:
 __version__ = "0.11.2024"
 __author__ = "Damien Altenburg"
 
-def format_greeting(first_name: str, last_name: str, salutation="Hello", title="Mr.") -> str:
+def format_greeting(first_name: str, last_name: str, 
+                    salutation="Hello", title="Mr.") -> str:
+    """Returns a formatted greeting using the specified person's name.
+    
+    Args:
+        first_name (str): The person's first name.
+        last_name (str): The person's last name.
+        salutation (str): The salutation part of the greeting. 
+        The default is "Hello".
+        title (str): The person's title. The default is "Mr.".
+        
+    Returns:
+        str: A formatted greeting using the specified person's name.
+    """
     first_name = first_name.strip()
 
     if first_name != "":
@@ -16,6 +29,18 @@ def format_greeting(first_name: str, last_name: str, salutation="Hello", title="
     return f"{salutation}, {title} {first_name}{last_name}"
 
 def to_binary(decimal_number: int) -> str:
+    """Returns the binary equivalent for the specified decimal number.
+
+    Args:
+        decimal_number (int): The decimal number to convert to a
+        binary number.
+
+    Returns:
+        str: The binary equivalent for the specified decimal number.
+    """
+    if not isinstance(decimal_number, int):
+        raise TypeError("The decimal number must be int.")
+
     binary_number = ""
 
     while decimal_number > 0:
@@ -29,3 +54,5 @@ def to_binary(decimal_number: int) -> str:
         binary_number = str(binary_digit) + binary_number
 
     return binary_number
+
+print(to_binary(127))
