c5ecd96e674070c0460d5582ec4ab06fb355037f
Damien Altenburg <daltenburg@rrc.ca>
2024-10-22 11:07:37 -0500

    Update to demonstrate local vs. global scope.

--- module-5/variable_scope.py
+++ module-5/variable_scope.py
@@ -7,8 +7,16 @@ Example:
 __version__ = "0.11.2024"
 __author__ = "Damien Altenburg"
 
+name = "Damien"
+
+decimal_number = 0
+binary_number = ""
+
 def to_binary(decimal_number: int) -> str:
     binary_number = ""
+
+    # Global scope
+    print(name)
 
     while decimal_number > 0:
         # The binary digit is the remainder of dividing by 2
@@ -23,3 +31,9 @@ def to_binary(decimal_number: int) -> str:
     return binary_number
 
 print(to_binary(147))
+
+# Global scope
+print(name)
+
+print(decimal_number)
+print(binary_number)
