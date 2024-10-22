c9bb12ba9b533da163f54ddfd64a000a7d8db882
Damien Altenburg <daltenburg@rrc.ca>
2024-10-21 19:54:41 -0500

    Update source examples.

--- module-5/function_basics.py
+++ module-5/function_basics.py
@@ -16,6 +16,9 @@ __author__ = "Damien Altenburg"
 def print_greeting(first_name: str, last_name: str) -> None:
     print(f"Hello, {first_name} {last_name}")
 
+def format_greeting(first_name: str, last_name: str) -> str:
+    return f"Hello, {first_name} {last_name}"
+
 print_greeting()
 
 print_greeting("Damien", "Altenburg")
@@ -23,3 +26,11 @@ print_greeting("Damien", "Altenburg")
 # Must satisfy parameter list
 # TypeError
 #print_greeting("Damien")
+
+greeting = format_greeting("Damien", "Altenburg")
+
+print(greeting)
+
+greeting = print_greeting("Damien", "Altenburg")
+
+print(greeting) # None
