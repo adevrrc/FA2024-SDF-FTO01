2e805785541e2e42c7c214beb09de97389977c7f
Damien Altenburg <daltenburg@rrc.ca>
2024-10-21 19:50:54 -0500

    Update to demonstrate declaring, defining, and invoking functions.

--- module-5/function_basics.py
+++ module-5/function_basics.py
@@ -6,3 +6,20 @@ Example:
 
 __version__ = "0.11.2024"
 __author__ = "Damien Altenburg"
+
+# Not defined error
+#print_greeting("Elvis", "Presley")
+
+# def print_greeting() -> None:
+#     print("Hello World")
+
+def print_greeting(first_name: str, last_name: str) -> None:
+    print(f"Hello, {first_name} {last_name}")
+
+print_greeting()
+
+print_greeting("Damien", "Altenburg")
+
+# Must satisfy parameter list
+# TypeError
+#print_greeting("Damien")
