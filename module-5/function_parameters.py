b1b2b388a72fa73a6d77010e5e22d997639bfd51
Damien Altenburg <daltenburg@rrc.ca>
2024-10-21 19:51:42 -0500

    Update to demonstrate positional, required, and optional parameters.

--- module-5/function_parameters.py
+++ module-5/function_parameters.py
@@ -6,3 +6,30 @@ Example:
 
 __version__ = "0.11.2024"
 __author__ = "Damien Altenburg"
+
+# def format_greeting(first_name: str, last_name: str) -> str:
+#     return f"Hello, {first_name} {last_name}"
+
+# Default Values
+# def format_greeting(first_name="world", last_name=""):
+#     return f"Hello, {first_name} {last_name}"
+
+def format_greeting(first_name: str, last_name: str, 
+                    salutation="Hello", title="Mr.") -> str:
+    return f"{salutation}, {title} {first_name} {last_name}"
+
+# Positional Arguments
+print(format_greeting("Damien", "Altenburg"))
+
+# Keyword Arguments
+print(format_greeting(first_name="Damien", last_name="Altenburg"))
+print(format_greeting(last_name="Warkentin", first_name="Deron"))
+
+# Positional Arguments are required
+# TypeError
+#print(format_greeting(last_name="Altenburg"))
+
+print(format_greeting("Damien", "Altenburg"))
+print(format_greeting("Damien", "Altenburg", "Bonjour"))
+print(format_greeting("Jane", "Doe", "Bonjour", "Mrs."))
+print(format_greeting("Jane", "Doe", title="Dr."))
