52762b58786c24e2fb5e9db584dd30ffc4c7986a
Damien Altenburg <daltenburg@rrc.ca>
2024-11-04 14:05:49 -0600

    Update to declare and define the PlayingCard class.

--- module-6/main.py
+++ module-6/main.py
@@ -3,8 +3,16 @@
 __author__ = "Damien Altenburg"
 __version__ = "0.11.2024"
 
+from playing_card.playing_card import PlayingCard
+
 def main():
-    pass
+    # ref_var = Constructor([arg])
+    card = PlayingCard(1, "Heart")
+
+    print(card)
+
+    #print(card.__rank)
+    #print(card.__suit)
 
 if __name__ == "__main__":
     main()
--- module-6/playing_card/playing_card.py
+++ module-6/playing_card/playing_card.py
@@ -2,4 +2,23 @@
 
 __author__ = "Damien Altenburg"
 __version__ = "0.11.2024"
-    
+
+class PlayingCard:
+    """Represents a standard playing card."""
+
+    def __init__(self, rank: int, suit: str):
+        """"""
+        self.__suit = suit
+        self.__rank = rank
+
+    """def get_suit():
+        pass
+
+    def set_suit(suit):
+        pass
+
+    def get_rank():
+        pass
+
+    def set_rank(suit):
+        pass"""
